7f736214c2c2dc01825789997245284203c2298b
Kozhevnikova0910 <kozhevnikova0910@gmail.com>
2023-07-14 05:24:53 +0300

    fix: fix tests

--- controllers/users.js
+++ controllers/users.js
@@ -29,14 +29,14 @@ module.exports.createUser = (req, res, next) => {
 
 module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true, })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch(next);
 };
 
 module.exports.updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true, })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch(next);
 };
--- errorHandler.js
+++ errorHandler.js
@@ -4,15 +4,15 @@ const COMMON_ERROR_CODE = 500;
 
 const errorHandler = (err, req, res, next) => {
   if (err.name === 'CastError') {
-    res.status(BAD_REQUEST).send( { message: err.message } );
+    res.status(BAD_REQUEST).send({ message: err.message });
   }
   if (err.name === 'NotFoundError') {
-    res.status(NOT_FOUND_CODE).send( { message: err.message } );
+    res.status(NOT_FOUND_CODE).send({ message: err.message });
   }
   if (err.name === 'ValidationError') {
-    res.status(BAD_REQUEST).send( { "message": err.message } );
+    res.status(BAD_REQUEST).send({ message: err.message });
   } else {
-    res.status(COMMON_ERROR_CODE).send( { "message": err.message } );
+    res.status(COMMON_ERROR_CODE).send({ message: err.message });
   }
   next();
 };
