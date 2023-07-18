9d16ef1425b1436fa5ed44cac4ae1f5a0ea42b65
Kozhevnikova0910 <kozhevnikova0910@gmail.com>
2023-07-18 03:17:40 +0300

    feat: 14 sprint start

--- errorHandler.js
+++ errorHandler.js
@@ -1,27 +1,4 @@
-// const BAD_REQUEST = 400;
-// const UNAUTHORIZED = 401;
-// const NOT_FOUND_CODE = 404;
-// const COMMON_ERROR_CODE = 500;
-//
-// const errorHandler = (err, req, res, next) => {
-//   if (err.name === 'CastError') {
-//     res.status(BAD_REQUEST).send({ message: err.message });
-//   }
-//   if (err.name === 'Unauthorized') {
-//     res.status(UNAUTHORIZED).send({ message: err.message });
-//   }
-//   if (err.name === 'NotFoundError') {
-//     res.status(NOT_FOUND_CODE).send({ message: err.message });
-//   }
-//   if (err.name === 'ValidationError') {
-//     res.status(BAD_REQUEST).send({ message: err.message });
-//   } else {
-//     res.status(COMMON_ERROR_CODE).send({ message: 'На сервере произошла ошибка' });
-//   }
-//   next();
-// };
-
-module.exports.errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res
@@ -34,3 +11,5 @@ module.exports.errorHandler = (err, req, res, next) => {
 
   next();
 }
+
+module.exports = errorHandler;
